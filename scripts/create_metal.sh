8002004a359bd63a29fc3e4e3722d4be52927f3a
Jaysonzhao <jaysonzhao@vip.qq.com>
2024-12-09 08:48:31 +0800

    Create create_metal.sh

---
+++ scripts/create_metal.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+unset $1
+unset $2
+
+set -o nounset
+set -o errexit
+set -o pipefail
+
+# Get machineset name to generate a generic template
+export ref_machineset_name=$(oc get machinesets -n openshift-machine-api |grep worker |awk '{ print $1 }')
+
+# Replace machine name worker to gpu
+export metal_machineset_name=$(echo $ref_machineset_name | sed s/worker/metal/)
+
+# Replace machine replicas number
+export NODE_NO=$(echo "${1:-2}")
+
+# Desired GPU instance type
+export instance_type=$(echo "${2:-m5zn.metal}")	
+
+dir=/tmp/metal-ci
+mkdir -p "${dir}/"
+
+# Get a templated json from a running machine, change machine type and machine name
+# and pass it to oc to create a new machine set
+set +o errexit
+oc get -n openshift-machine-api machineset $ref_machineset_name -o json \
+    | jq 'del(.status)|del(.metadata.selfLink)|del(.metadata.uid)' \
+    | jq --arg instance_type "${instance_type}" '.spec.template.spec.providerSpec.value.instanceType = $instance_type' \
+    | jq --arg metal_machineset_name "${metal_machineset_name}" '.metadata.name = $metal_machineset_name' \
+    | jq --arg metal_machineset_name "${metal_machineset_name}" '.spec.template.metadata.labels."machine.openshift.io/cluster-api-machineset" = $metal_machineset_name' \
+    | jq --arg metal_machineset_name "${metal_machineset_name}" '.spec.selector.matchLabels."machine.openshift.io/cluster-api-machineset" = $metal_machineset_name' \
+    | jq --argjson NODE_NO "${NODE_NO}" '.spec.replicas = $NODE_NO' \
+    | oc create -f-
+ecode=$?
+set -o errexit
